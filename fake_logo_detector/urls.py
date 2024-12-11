50637094349592ae9f748d053ae6095ff6ba03a8
Andro-ux <vinitphanse@gmail.com>
2024-12-11 13:03:54 +0530

    made changes

---
+++ fake_logo_detector/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for fake_logo_detector project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.index, name='index')
+Class-based views
+    1. Add an import:  from other_app.views import index
+    2. Add a URL to urlpatterns:  path('', index.as_view(), name='index')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# fake_logo_detector/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('detector.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
